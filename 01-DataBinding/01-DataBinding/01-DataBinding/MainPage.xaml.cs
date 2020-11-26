b6031bf57134441bce7caef7cde993bb3eff7e40
Sayed Mahmood Ghaemmaghami <63294201+mahmood-ghaem@users.noreply.github.com>
2020-11-26 02:09:40 +0100

    01-DataBinding-Step01

---
+++ 01-DataBinding/01-DataBinding/01-DataBinding/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace _01_DataBinding
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            slider1.Value = 0.5;
+            slider2.Value = 0.5;
+        }
+
+        private void slider1_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            label1.Text = string.Format("Value is {0:F2}", slider1.Value);
+        }
+    }
+}
